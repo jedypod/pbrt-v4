jedypod/pbrt-v4

commit 38d332e0930b74a0d683dc13ccb05b47d3802ffe

Add double-precision variants of SafeACos() et al.

src/util/mathutil.h
@@ -263,14 +263,25 @@ inline constexpr Float Lerp(Float t, Float v1, Float v2) {
     return (1 - t) * v1 + t * v2;
 }
 
-inline constexpr Float Sqr(Float v) { return v * v; }
+template <typename T>
+inline constexpr Float Sqr(T v) { return v * v; }
 
-inline Float SafeASin(Float x) {
+inline float SafeASin(float x) {
     DCHECK(x >= -1.0001 && x <= 1.0001);
     return std::asin(Clamp(x, -1, 1));
 }
 
-inline Float SafeACos(Float x) {
+inline double SafeASin(double x) {
+    DCHECK(x >= -1.0001 && x <= 1.0001);
+    return std::asin(Clamp(x, -1, 1));
+}
+
+inline float SafeACos(float x) {
+    DCHECK(x >= -1.0001 && x <= 1.0001);
+    return std::acos(Clamp(x, -1, 1));
+}
+
+inline double SafeACos(double x) {
     DCHECK(x >= -1.0001 && x <= 1.0001);
     return std::acos(Clamp(x, -1, 1));
 }
@@ -285,6 +296,8 @@ inline double SafeSqrt(double x) {
     return std::sqrt(std::max(0., x));
 }
 
+// Would be nice to allow Float to be a template type here, but it's tricky:
+// https://stackoverflow.com/questions/5101516/why-function-template-cannot-be-partially-specialized
 template <int n>
 inline constexpr Float Pow(Float v) {
     static_assert(n > 0, "Power can't be negative");

commit a607b79fa796f9e336693f90f7cb5eb86bb7b13e

Fix int overflow bug in HairMaterial
Introduced in 38d332e0930b74a0d683dc13ccb05b47d3802ffe

src/integrators/bdpt.cpp
@@ -335,6 +335,7 @@ void BDPTIntegrator::Render(const Scene &scene) {
     // Render and write the output image to disk
     if (scene.lights.size() > 0) {
         ParallelFor2D(sampleBounds, tileSize, [&](Bounds2i tileBounds) {
+            LOG(INFO) << "Starting tile " << tileBounds;
             // Render a single tile using BDPT
             MemoryArena arena;
             std::unique_ptr<Sampler> tileSampler = sampler->Clone();

src/materials/hair.cpp
@@ -69,7 +69,7 @@ static Float Mp(Float cosThetaI, Float cosThetaO, Float sinThetaI,
 inline Float I0(Float x) {
     Float val = 0;
     Float x2i = 1;
-    int ifact = 1;
+    int64_t ifact = 1;
     int i4 = 1;
     // I0(x) \approx Sum_i x^(2i) / (4^i (i!)^2)
     for (int i = 0; i < 10; ++i) {

src/util/mathutil.h
@@ -264,7 +264,7 @@ inline constexpr Float Lerp(Float t, Float v1, Float v2) {
 }
 
 template <typename T>
-inline constexpr Float Sqr(T v) { return v * v; }
+inline constexpr T Sqr(T v) { return v * v; }
 
 inline float SafeASin(float x) {
     DCHECK(x >= -1.0001 && x <= 1.0001);