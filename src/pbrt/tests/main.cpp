jedypod/pbrt-v4

commit b41055007793955667ad5935d43db4b73775a7dd

util/parallel cleanup

src/pbrt/util/parallel.cpp
@@ -34,10 +34,13 @@
 // core/parallel.cpp*
 #include <pbrt/util/parallel.h>
 
-#include <pbrt/util/memory.h>
+#include <pbrt/util/bounds.h>
 #include <pbrt/util/stats.h>
 
 #include <absl/synchronization/barrier.h>
+#include <glog/logging.h>
+
+#include <condition_variable>
 #include <list>
 #include <thread>
 #include <vector>
@@ -46,50 +49,130 @@ namespace pbrt {
 
 class ParallelJob {
   public:
-    static void StartThreads(int nThreads);
-    static void TerminateThreads();
-
     virtual ~ParallelJob() { DCHECK(removed); }
 
-    std::unique_lock<std::mutex> AddToJobList();
     // *lock should be locked going in and and unlocked coming out.
     virtual void RunStep(std::unique_lock<std::mutex> *lock) = 0;
-
     virtual bool HaveWork() const = 0;
+
     bool Finished() const { return !HaveWork() && activeWorkers == 0; }
-    static size_t NumThreads() { return threads.size(); }
-
-    static void DoWork(std::unique_lock<std::mutex> &lock);
-
-protected:
-    void RemoveFromJobList();
 
 private:
-    static void workerFunc(int tIndex, absl::Barrier *barrier);
-
-    static ParallelJob *jobList;
-    // Protects jobList
-    static std::mutex jobListMutex;
-    // Signaled both when a new job is added to the list and when a job has
-    // finished.
-    static std::condition_variable jobListCondition;
-
-    static std::vector<std::thread> threads;
-    static bool shutdownThreads;
+    friend class ThreadPool;
 
     ParallelJob *prev = nullptr, *next = nullptr;
     int activeWorkers = 0;
     bool removed = false;
 };
 
-ParallelJob *ParallelJob::jobList = nullptr;
-std::mutex ParallelJob::jobListMutex;
-std::condition_variable ParallelJob::jobListCondition;
+class ThreadPool {
+  public:
+    ThreadPool(int nThreads);
+    ~ThreadPool();
 
-std::vector<std::thread> ParallelJob::threads;
-bool ParallelJob::shutdownThreads = false;
+    size_t size() const { return threads.size(); }
 
-void ParallelJob::DoWork(std::unique_lock<std::mutex> &lock) {
+    std::unique_lock<std::mutex> AddToJobList(ParallelJob *job);
+    void RemoveFromJobList(ParallelJob *job);
+
+    void WorkOrWait(std::unique_lock<std::mutex> &lock);
+
+    void ForEachWorkerThread(std::function<void(void)> func);
+
+  private:
+    void workerFunc(int tIndex, absl::Barrier *barrier);
+
+    ParallelJob *jobList = nullptr;
+    // Protects jobList
+    std::mutex jobListMutex;
+    // Signaled both when a new job is added to the list and when a job has
+    // finished.
+    std::condition_variable jobListCondition;
+
+    std::vector<std::thread> threads;
+    bool shutdownThreads = false;
+};
+
+thread_local int ThreadIndex;
+
+static std::unique_ptr<ThreadPool> threadPool;
+static bool maxThreadIndexCalled = false;
+
+ThreadPool::ThreadPool(int nThreads) {
+    ThreadIndex = 0;
+
+    // Create a barrier so that we can be sure all worker threads get past
+    // their call to ProfilerWorkerThreadInit() before we return from this
+    // function.  In turn, we can be sure that the profiling system isn't
+    // started until after all worker threads have done that.
+    absl::Barrier *barrier = new absl::Barrier(nThreads);
+
+    // Launch one fewer worker thread than the total number we want doing
+    // work, since the main thread helps out, too.
+    for (int i = 0; i < nThreads - 1; ++i)
+        threads.push_back(std::thread(&ThreadPool::workerFunc, this, i + 1,
+                                      barrier));
+
+    if (barrier->Block()) delete barrier;
+}
+
+ThreadPool::~ThreadPool() {
+    if (threads.empty()) return;
+
+    {
+        std::lock_guard<std::mutex> lock(jobListMutex);
+        shutdownThreads = true;
+        jobListCondition.notify_all();
+    }
+
+    for (std::thread &thread : threads) thread.join();
+}
+
+std::unique_lock<std::mutex> ThreadPool::AddToJobList(ParallelJob *job) {
+    std::unique_lock<std::mutex> lock(jobListMutex);
+    if (jobList) jobList->prev = job;
+    job->next = jobList;
+    jobList = job;
+    jobListCondition.notify_all();
+    return lock;
+}
+
+void ThreadPool::RemoveFromJobList(ParallelJob *job) {
+    DCHECK(!job->removed);
+
+    if (job->prev) {
+        job->prev->next = job->next;
+    } else {
+        DCHECK(jobList == job);
+        jobList = job->next;
+    }
+    if (job->next)
+        job->next->prev = job->prev;
+
+    job->removed = true;
+}
+
+void ThreadPool::workerFunc(int tIndex, absl::Barrier *barrier) {
+    LOG(INFO) << "Started execution in worker thread " << tIndex;
+    ThreadIndex = tIndex;
+
+    // Give the profiler a chance to do per-thread initialization for
+    // the worker thread before the profiling system actually stops running.
+    ProfilerWorkerThreadInit();
+
+    // The main thread sets up a barrier so that it can be sure that all
+    // workers have called ProfilerWorkerThreadInit() before it continues
+    // (and actually starts the profiling system).
+    if (barrier->Block()) delete barrier;
+
+    std::unique_lock<std::mutex> lock(jobListMutex);
+    while (!shutdownThreads)
+        WorkOrWait(lock);
+
+    LOG(INFO) << "Exiting worker thread " << tIndex;
+}
+
+void ThreadPool::WorkOrWait(std::unique_lock<std::mutex> &lock) {
     DCHECK(lock.owns_lock());
 
     ParallelJob *job = jobList;
@@ -97,8 +180,6 @@ void ParallelJob::DoWork(std::unique_lock<std::mutex> &lock) {
         job = job->next;
     if (job) {
         // Run a chunk of loop iterations for _loop_
-        CHECK(job->HaveWork());
-
         job->activeWorkers++;
 
         job->RunStep(&lock);
@@ -114,32 +195,21 @@ void ParallelJob::DoWork(std::unique_lock<std::mutex> &lock) {
     } else
         // Wait for something to change (new work, or this loop being
         // finished).
-        ParallelJob::jobListCondition.wait(lock);
+        jobListCondition.wait(lock);
 }
 
-std::unique_lock<std::mutex> ParallelJob::AddToJobList() {
-    if (threads.size() == 0 && MaxThreadIndex() > 1)
-        LOG(WARNING) << "Threads not launched; job will run serially";
+void ThreadPool::ForEachWorkerThread(std::function<void(void)> func) {
+    absl::Barrier *barrier = new absl::Barrier(threads.size());
 
-    std::unique_lock<std::mutex> lock(jobListMutex);
-    if (jobList) jobList->prev = this;
-    next = jobList;
-    jobList = this;
-    jobListCondition.notify_all();
-    return lock;
+    ParallelFor(0, threads.size(),
+                [barrier,&func](int64_t) {
+                    func();
+                    if (barrier->Block()) delete barrier;
+                });
 }
 
-void ParallelJob::RemoveFromJobList() {
-    if (prev) {
-        prev->next = next;
-    } else {
-        DCHECK(jobList == this);
-        jobList = next;
-    }
-    if (next)
-        next->prev = prev;
-    removed = true;
-}
+///////////////////////////////////////////////////////////////////////////
+// ParallelJob
 
 class ParallelForLoop1D : public ParallelJob {
   public:
@@ -194,7 +264,7 @@ void ParallelForLoop1D::RunStep(std::unique_lock<std::mutex> *lock) {
     nextIndex = indexEnd;
 
     if (!HaveWork())
-        RemoveFromJobList();
+        threadPool->RemoveFromJobList(this);
 
     lock->unlock();
 
@@ -221,7 +291,7 @@ void ParallelForLoop2D::RunStep(std::unique_lock<std::mutex> *lock) {
     }
 
     if (!HaveWork())
-        RemoveFromJobList();
+        threadPool->RemoveFromJobList(this);
 
     lock->unlock();
 
@@ -234,26 +304,6 @@ void ParallelForLoop2D::RunStep(std::unique_lock<std::mutex> *lock) {
     ProfilerState = oldState;
 }
 
-void ParallelJob::workerFunc(int tIndex, absl::Barrier *barrier) {
-    LOG(INFO) << "Started execution in worker thread " << tIndex;
-    ThreadIndex = tIndex;
-
-    // Give the profiler a chance to do per-thread initialization for
-    // the worker thread before the profiling system actually stops running.
-    ProfilerWorkerThreadInit();
-
-    // The main thread sets up a barrier so that it can be sure that all
-    // workers have called ProfilerWorkerThreadInit() before it continues
-    // (and actually starts the profiling system).
-    if (barrier->Block()) delete barrier;
-
-    std::unique_lock<std::mutex> lock(jobListMutex);
-    while (!shutdownThreads)
-        DoWork(lock);
-
-    LOG(INFO) << "Exiting worker thread " << tIndex;
-}
-
 void ParallelFor(int64_t start, int64_t end, int chunkSize,
                  std::function<void(int64_t, int64_t)> func) {
     if (end - start < chunkSize) {
@@ -264,14 +314,15 @@ void ParallelFor(int64_t start, int64_t end, int chunkSize,
     // Create and enqueue _ParallelJob_ for this loop
     ParallelForLoop1D loop(start, end, chunkSize, std::move(func),
                            CurrentProfilerState());
-    std::unique_lock<std::mutex> lock = loop.AddToJobList();
+    std::unique_lock<std::mutex> lock = threadPool->AddToJobList(&loop);
 
     // Help out with parallel loop iterations in the current thread
     while (!loop.Finished())
-        ParallelJob::DoWork(lock);
+        threadPool->WorkOrWait(lock);
 }
 
-void ParallelFor2D(const Bounds2i &extent, int chunkSize, std::function<void(Bounds2i)> func) {
+void ParallelFor2D(const Bounds2i &extent, int chunkSize,
+                   std::function<void(Bounds2i)> func) {
     if (extent.Empty())
         return;
 
@@ -282,84 +333,43 @@ void ParallelFor2D(const Bounds2i &extent, int chunkSize, std::function<void(Bou
 
     ParallelForLoop2D loop(extent, chunkSize, std::move(func),
                            CurrentProfilerState());
-    std::unique_lock<std::mutex> lock = loop.AddToJobList();
+    std::unique_lock<std::mutex> lock = threadPool->AddToJobList(&loop);
 
     // Help out with parallel loop iterations in the current thread
     while (!loop.Finished())
-        ParallelJob::DoWork(lock);
+        threadPool->WorkOrWait(lock);
 }
 
 ///////////////////////////////////////////////////////////////////////////
-
-thread_local int ThreadIndex;
-static bool parallelInitialized = false;
 
 int AvailableCores() {
     return std::max<int>(1, std::thread::hardware_concurrency());
 }
 
 int MaxThreadIndex() {
-    return parallelInitialized ? (1 + ParallelJob::NumThreads()) : 1;
+    maxThreadIndexCalled = true;
+    return threadPool ? (1 + threadPool->size()) : 1;
 }
 
 void ParallelInit(int nThreads) {
-    CHECK(!parallelInitialized);
-    parallelInitialized = true;
+    // This is risky: if the caller has allocated per-thread data
+    // structures before calling ParallelInit(), then we may end up having
+    // them accessed with a higher ThreadIndex than the caller expects.
+    CHECK(!maxThreadIndexCalled);
+
+    CHECK(!threadPool);
     if (nThreads <= 0)
         nThreads = AvailableCores();
-    ParallelJob::StartThreads(nThreads);
-}
-
-void ParallelJob::StartThreads(int nThreads) {
-    CHECK_EQ(threads.size(), 0);
-    ThreadIndex = 0;
-
-    // Create a barrier so that we can be sure all worker threads get past
-    // their call to ProfilerWorkerThreadInit() before we return from this
-    // function.  In turn, we can be sure that the profiling system isn't
-    // started until after all worker threads have done that.
-    absl::Barrier *barrier = new absl::Barrier(nThreads);
-
-    // Launch one fewer worker thread than the total number we want doing
-    // work, since the main thread helps out, too.
-    for (int i = 0; i < nThreads - 1; ++i)
-        threads.push_back(std::thread(workerFunc, i + 1, barrier));
-
-    if (barrier->Block()) delete barrier;
+    threadPool = std::make_unique<ThreadPool>(nThreads);
 }
 
 void ParallelCleanup() {
-    ParallelJob::TerminateThreads();
-    parallelInitialized = false;
-}
-
-void ParallelJob::TerminateThreads() {
-    if (threads.empty()) return;
-
-    {
-        std::lock_guard<std::mutex> lock(jobListMutex);
-        shutdownThreads = true;
-        jobListCondition.notify_all();
-    }
-
-    for (std::thread &thread : threads) thread.join();
-    threads.erase(threads.begin(), threads.end());
-    shutdownThreads = false;
+    threadPool.reset();
+    maxThreadIndexCalled = false;
 }
 
 void ForEachWorkerThread(std::function<void(void)> func) {
-    int nThreads = MaxThreadIndex();
-    absl::Barrier *barrier = new absl::Barrier(nThreads);
-
-    ParallelFor(0, nThreads,
-                [barrier,&func](int64_t) {
-                    func();
-                    if (barrier->Block()) delete barrier;
-                });
-}
-
-void MergeWorkerThreadStats() {
-    ForEachWorkerThread(ReportThreadStats);
+    threadPool->ForEachWorkerThread(std::move(func));
 }
 
 }  // namespace pbrt

src/pbrt/core/api.cpp
@@ -1582,8 +1582,9 @@ void pbrtWorldEnd() {
     ImageTexture<Float>::ClearCache();
     ImageTexture<Spectrum>::ClearCache();
 
-    MergeWorkerThreadStats();
+    ForEachWorkerThread(ReportThreadStats);
     ReportThreadStats();
+
     if (!PbrtOptions.quiet) {
         PrintStats(stdout);
         ReportProfilerResults(stdout);

src/pbrt/util/parallel.h
@@ -43,12 +43,8 @@
 
 #include <pbrt/util/bounds.h>
 #include <pbrt/util/math.h>
-#include <pbrt/util/geometry.h>
-#include <pbrt/util/stats.h>
-#include <glog/logging.h>
 
 #include <atomic>
-#include <condition_variable>
 #include <cstdint>
 #include <functional>
 #include <mutex>
@@ -96,24 +92,6 @@ inline void ParallelFor2D(const Bounds2i &extent,
     ParallelFor2D(extent, 1, std::move(func));
 }
 
-template <typename T, typename F, typename R> T
-ParallelReduce(int64_t start, int64_t end, int chunkSize, F func, R reduce) {
-    if (start == end) return T{};
-    if (end - start < chunkSize) {
-        return func(start, end);
-    } else {
-        std::mutex mutex;
-        T final;
-        ParallelFor(start, end, chunkSize,
-                    [&](int64_t start, int64_t end) {
-                        T result = func(start, end);
-                        std::lock_guard<std::mutex> lock(mutex);
-                        reduce(final, result);
-                    });
-        return final;
-    }
-}
-
 void ForEachWorkerThread(std::function<void(void)> func);
 
 extern thread_local int ThreadIndex;
@@ -123,7 +101,6 @@ int MaxThreadIndex();
 
 void ParallelInit(int nThreads = -1);
 void ParallelCleanup();
-void MergeWorkerThreadStats();
 
 }  // namespace pbrt
 

commit 533f51cfdf32f8d642dc118562403af173fc75e5

Improve parallel initialization in pbrt_test
Implement our own main() so that we can initialize and clean that up
properly and consistently. (Also, fixes tests that broke in
b41055007793955667ad5935d43db4b73775a7dd)

CMakeLists.txt
@@ -387,7 +387,6 @@ FILE ( GLOB PBRT_SOURCE
   src/pbrt/util/*.cpp
 
   src/ext/gtest/gtest-all.cc
-  src/ext/gtest/gtest_main.cc
   src/ext/lodepng/lodepng.cpp
   src/ext/rply/rply.cpp
   src/ext/skymodel/ArHosekSkyModel.c

src/pbrt/util/parallel.cpp
@@ -306,6 +306,7 @@ void ParallelForLoop2D::RunStep(std::unique_lock<std::mutex> *lock) {
 
 void ParallelFor(int64_t start, int64_t end, int chunkSize,
                  std::function<void(int64_t, int64_t)> func) {
+    CHECK(threadPool);
     if (end - start < chunkSize) {
         func(start, end);
         return;
@@ -323,6 +324,7 @@ void ParallelFor(int64_t start, int64_t end, int chunkSize,
 
 void ParallelFor2D(const Bounds2i &extent, int chunkSize,
                    std::function<void(Bounds2i)> func) {
+    CHECK(threadPool);
     if (extent.Empty())
         return;
 

src/pbrt/tests/analytic_scenes.cpp
@@ -423,10 +423,6 @@ std::vector<TestIntegrator> GetIntegrators() {
 struct RenderTest : public testing::TestWithParam<TestIntegrator> {};
 
 TEST_P(RenderTest, RadianceMatches) {
-    Options options;
-    options.quiet = true;
-    pbrtInit(options);
-
     const TestIntegrator &tr = GetParam();
     tr.integrator->Render();
     CheckSceneAverage(inTestDir("test.exr"), tr.scene.expected);
@@ -435,8 +431,6 @@ TEST_P(RenderTest, RadianceMatches) {
     // must delete the Integrator here in order to make sure that its
     // destructor runs. (This is ugly and should be fixed in a better way.)
     delete tr.integrator;
-
-    pbrtCleanup();
 
     EXPECT_EQ(0, remove(inTestDir("test.exr").c_str()));
 }

src/pbrt/tests/bsdfs.cpp
@@ -376,9 +376,6 @@ void TestBSDF(void (*createBSDF)(BSDF*, MemoryArena&),
               const char* description) {
     MemoryArena arena;
 
-    Options opt;
-    pbrtInit(opt);
-
     const int thetaRes = CHI2_THETA_RES;
     const int phiRes = CHI2_PHI_RES;
     const int sampleCount = CHI2_SAMPLECOUNT;
@@ -437,8 +434,6 @@ void TestBSDF(void (*createBSDF)(BSDF*, MemoryArena&),
 
     delete[] frequencies;
     delete[] expFrequencies;
-
-    pbrtCleanup();
 }
 
 void createLambertian(BSDF* bsdf, MemoryArena& arena) {

src/pbrt/tests/main.cpp
@@ -1,0 +1,55 @@
+
+/*
+    pbrt source code is Copyright(c) 1998-2016
+                        Matt Pharr, Greg Humphreys, and Wenzel Jakob.
+
+    This file is part of pbrt.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are
+    met:
+
+    - Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    - Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+    IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+    TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+    HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ */
+
+#include <pbrt/core/pbrt.h>
+
+#include <pbrt/core/api.h>
+#include <pbrt/core/options.h>
+
+#include <gtest/gtest.h>
+#include <glog/logging.h>
+
+int main(int argc, char **argv) {
+    pbrt::Options opt;
+    opt.quiet = true;
+    opt.nThreads = 8;
+    pbrt::pbrtInit(opt);
+
+    google::InitGoogleLogging(argv[0]);
+    FLAGS_stderrthreshold = 1; // Warning and above.
+    testing::InitGoogleTest(&argc, argv);
+    int ret = RUN_ALL_TESTS();
+
+    pbrt::pbrtCleanup();
+
+    return ret;
+}

src/pbrt/tests/parallel.cpp
@@ -7,8 +7,6 @@
 using namespace pbrt;
 
 TEST(Parallel, Basics) {
-    ParallelInit(8);
-
     std::atomic<int> counter{0};
     ParallelFor(0, 1000, [&](int64_t) { ++counter; });
     EXPECT_EQ(1000, counter);
@@ -27,13 +25,9 @@ TEST(Parallel, Basics) {
     counter = 0;
     ParallelFor2D(Bounds2i{{0, 0}, {15, 14}}, [&](Bounds2i b) { ++counter; });
     EXPECT_EQ(15*14, counter);
-
-    ParallelCleanup();
 }
 
 TEST(Parallel, DoNothing) {
-    ParallelInit(8);
-
     std::atomic<int> counter{0};
     ParallelFor(0, 0, [&](int64_t) { ++counter; });
     EXPECT_EQ(0, counter);
@@ -41,6 +35,4 @@ TEST(Parallel, DoNothing) {
     counter = 0;
     ParallelFor2D(Bounds2i{{0, 0}, {0, 0}}, 1, [&](Bounds2i b) { ++counter; });
     EXPECT_EQ(0, counter);
-
-    ParallelCleanup();
 }

src/pbrt/tests/texcache.cpp
@@ -76,8 +76,6 @@ static Image GetImage(PixelFormat format, Point2i res) {
 // then, add it to a texture cache and verify that all texels in all
 // MIP levels exactly match the values in the original MIP chain.
 static void TestFormat(PixelFormat format) {
-    ParallelInit(8);
-
     std::unique_ptr<TextureCache> cache = std::make_unique<TextureCache>();
     Image image = GetImage(format, {129, 60});
 
@@ -113,7 +111,6 @@ static void TestFormat(PixelFormat format) {
             }
     }
     EXPECT_EQ(0, remove(filename));
-    ParallelCleanup();
 }
 
 TEST(Texcache, SY8) { TestFormat(PixelFormat::SY8); }
@@ -133,8 +130,6 @@ TEST(Texcache, RGB16) { TestFormat(PixelFormat::RGB16); }
 TEST(Texcache, RGB32) { TestFormat(PixelFormat::RGB32); }
 
 TEST(Texcache, ThreadInsanity) {
-    ParallelInit(8);
-
     // Create a bunch of images with random sizes and contents.
     std::vector<std::vector<Image>> images;
     std::vector<std::string> filenames;
@@ -180,6 +175,4 @@ TEST(Texcache, ThreadInsanity) {
         });
 
     for (const auto &fn : filenames) EXPECT_EQ(0, remove(fn.c_str()));
-
-    ParallelCleanup();
 }